ca0c5fdd0285fe43662c037d9aab0b822c842c82
Unkqwn <unlqwn@hotmail.com>
2025-12-08 23:16:40 +0100

    Round manager werkt nu gedeeltelijk

--- XRpedition 2/Assets/Devs/Robin/Scripts/ManageRounds.cs
+++ XRpedition 2/Assets/Devs/Robin/Scripts/ManageRounds.cs
@@ -1,36 +1,73 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ManageRounds : MonoBehaviour
 {
     [SerializeField] private Generation generation;
     [SerializeField] private AnimalSelection animal;
+    [SerializeField] private SoundManager _soundManager;
 
-    private int round = 0;
-    private bool roundActive;
+    private Animals animals;
+    [SerializeField] private AnswerTesting answer;
+
+    private int round = 1;
+    public bool roundActive;
+
+    private void Start()
+    {
+        StartRound();
+    }
 
     private void Update()
     {
-        StartRound();
-        if (roundActive)
+        if (round > 5)
         {
-            Round();
+            SceneManager.LoadScene("Main Menu");
         }
-        EndRound();
     }
 
     private void StartRound()
     {
-        round++;
+        answer.AnswerChecking();
         generation.Generate(animal);
+        roundActive = true;
+        RoundSounds();
     }
 
-    private void Round()
+    private void RoundSounds()
     {
-
+        switch (round)
+        {
+            case 1:
+                _soundManager.PlayPolarAmbience();
+                break;
+            case 2:
+                _soundManager.PlayAfricaAmbience();
+                break;
+            case 3:
+                _soundManager.PlayChainsawSound();
+                break;
+            case 4:
+                _soundManager.PlayJungleAmbience();
+                break;
+            case 5:
+                _soundManager.PlayPolarAmbience();
+                break;
+        }
     }
 
-    private void EndRound()
+    public void EndingRound()
     {
+        StartCoroutine(EndRound());
+    }
+
+    private IEnumerator EndRound()
+    {
+        yield return new WaitForSeconds(5);
+        roundActive = false;
         animal.GetEnvironment();
+        round++;
+        StartRound();
     }
 }
