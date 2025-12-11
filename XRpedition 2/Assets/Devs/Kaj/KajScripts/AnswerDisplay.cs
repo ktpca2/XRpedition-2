b5e36c245342ec721befc73314795cba5beb75f6
ktpca2 <kajlevidevries@gmail.com>
2025-12-11 09:32:42 +0100

    Push voor main

--- XRpedition 2/Assets/Devs/Kaj/KajScripts/AnswerDisplay.cs
+++ XRpedition 2/Assets/Devs/Kaj/KajScripts/AnswerDisplay.cs
@@ -1,13 +1,41 @@
-using System;
 using UnityEngine;
+using TMPro;
 
 public class AnswerDisplay : MonoBehaviour
 {
     public int RoundsCorrectDisplay = 0;
+    public TextMeshProUGUI roundsText;
+
+    private void Awake()
+    {
+        AnswerDisplay[] objs = FindObjectsOfType<AnswerDisplay>();
+        if (objs.Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        // Show "0 / 5" as soon as the scene starts
+        UpdateText();
+    }
 
     public void UpdateRoundsDisplay()
     {
         RoundsCorrectDisplay++;
-        Debug.Log(RoundsCorrectDisplay.ToString() + " rounds correct!");
+        UpdateText();
+        Debug.Log(RoundsCorrectDisplay + " rounds correct!");
+    }
+
+    private void UpdateText()
+    {
+        if (roundsText != null)
+        {
+            roundsText.text = $"{RoundsCorrectDisplay} / 5";
+        }
     }
 }
